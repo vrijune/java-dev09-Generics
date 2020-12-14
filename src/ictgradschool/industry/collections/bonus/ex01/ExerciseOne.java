a33fdaee5a5b007bc0acb1ff84e23347ea65ba39
Wen YU <wyu861@aucklanduni.ac.nz>
2020-12-14 16:29:27 +1300

    industry lab09

--- src/ictgradschool/industry/collections/bonus/ex01/ExerciseOne.java
+++ src/ictgradschool/industry/collections/bonus/ex01/ExerciseOne.java
@@ -19,14 +19,14 @@ public class ExerciseOne {
         List<Object> strings = new ArrayList<>();
         strings.add("Hello");
         strings.add("hello");
-        strings.add(Math.PI); // Umm.... this seems fishy...
+//        strings.add(Math.PI); // Umm.... this seems fishy...
         strings.add("HELLO");
 
         // Create a list of Integers
         List<Object> ints = new ArrayList<>();
         ints.add(3);
         ints.add(2);
-        ints.add("Not actually a number"); // Someone doesn't know how to code...
+//        ints.add("Not actually a number"); // Someone doesn't know how to code...
         ints.add(4624);
         ints.add(397);
 
@@ -34,7 +34,7 @@ public class ExerciseOne {
         filterList(strings, new UpperCaseStringPredicate());
 
         // Filter the list of Integers, removing any which aren't odd numbers
-        filterList(ints, new UpperCaseStringPredicate());
+        filterList(ints, new OddNumbersPredicate());
 
         // Print out the items left in the strings list - should just be "HELLO".
         System.out.println("Strings:");
